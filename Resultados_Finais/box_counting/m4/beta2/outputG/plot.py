f8a4db9f0b0fcea36da9906f7fceed46fd035f46
Felipe Moreira <felipemoreira.physics@gmail.com>
2025-10-04 15:17:07 -0300

    add

---
+++ Resultados_Finais/box_counting/m4/beta2/outputG/plot.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("G0.dat")
+plt.plot(data[:,0], data[:,1])
+plt.show()
+
+
+x = data[:,0]
+y = data[:,1]
+
+y = y/42
+x = (x+2)/4
+
+plt.plot(x,y)
+plt.show()
+
+f = open("Gnew.dat","w")
+for i in range(len(x)):
+    f.write(f"{x[i]} {y[i]}\n")
+f.close()
